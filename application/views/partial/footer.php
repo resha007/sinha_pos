65ec81213b890e93c085d39e2938480ae83429ee
resha007 <reshans777@gmail.com>
2024-01-27 09:32:45 +0530

    footer name change

--- application/views/partial/footer.php
+++ application/views/partial/footer.php
@@ -4,7 +4,7 @@
 	<div id="footer">
 		<div class="jumbotron push-spaces">
 			<strong><?php echo $this->lang->line('common_copyrights', date('Y')); ?> · 
-			<a href="https:www.infinizee.com" target="_blank">Infinizee</a>  · 
+			<a href="https://www.facebook.com/777techlk/" target="_blank">777 Tech LK</a>  · 
   			<?php echo $this->config->item('application_version'); ?> - <?php echo substr($this->config->item('commit_sha1'), 0, 6); ?></strong>.
 		</div>
 	</div>
